b8906a4b3699e5f7df6fdaaf41000d40041f10f3
João Paulo Arnold Barros <joaopaulo167@gmail.com>
2024-04-09 15:00:34 -0300

    format

---
+++ main.py
@@ -0,0 +1,66 @@
+from cryptography.fernet import Fernet
+
+from dotenv import load_dotenv
+import os
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+load_dotenv()
+
+
+MASTER_KEY = os.environ.get("MASTER_KEY")
+
+def write_key():
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    token = f.encrypt(MASTER_KEY.encode())
+    with open("key.key", "wb") as key_file:
+        key_file.write(token)
+
+# write_key()
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
